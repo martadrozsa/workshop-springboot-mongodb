186428174a0e4ac85833aedf84a20ff633da31dd
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-03 10:53:38 -0300

    Project created

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # workshop-springboot-mongodb
+
+Seção do curso
+###Java COMPLETO Programação Orientada a Objetos +Projetos
+
+- https://www.udemy.com/course/java-curso-completo/
---
+++ src/test/java/com/martadrozsa/workshopmongo/workshopspringbootmongodb/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martadrozsa.workshopmongo.workshopspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
