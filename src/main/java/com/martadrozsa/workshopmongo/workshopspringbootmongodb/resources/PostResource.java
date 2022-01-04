5ac983a4fc6252561c15700797263277b090e50d
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-04 18:09:14 -0300

    Add multi-criteria queries

--- src/main/java/com/martadrozsa/workshopmongo/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/martadrozsa/workshopmongo/workshopspringbootmongodb/resources/PostResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
 
 
@@ -33,5 +34,17 @@ public class PostResource {
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
         return ResponseEntity.ok().body((list));
+    }
+
+    @RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
     }
 }
