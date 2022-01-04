e44fd513b19cfe379600bf2b373aaef1d87a9acc
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-04 17:05:02 -0300

    Querying with Query

--- src/main/java/com/martadrozsa/workshopmongo/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/martadrozsa/workshopmongo/workshopspringbootmongodb/resources/PostResource.java
@@ -1,10 +1,14 @@
 package com.martadrozsa.workshopmongo.workshopspringbootmongodb.resources;
 
 import com.martadrozsa.workshopmongo.workshopspringbootmongodb.domain.Post;
+import com.martadrozsa.workshopmongo.workshopspringbootmongodb.resources.util.URL;
 import com.martadrozsa.workshopmongo.workshopspringbootmongodb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.util.List;
 
 
 @RestController
@@ -24,5 +28,10 @@ public class PostResource {
         return ResponseEntity.ok().body((obj));
     }
 
-
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body((list));
+    }
 }
